0cb4307f7be6054d2af1e635ca32a65624c3fa9c
elsam-mohamedl <ee.eslam355@gmail.com>
2025-09-06 19:17:52 +0300

    fix second error

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import {RouterProvider } from 'react-router-dom';
 import AppRouter from './routes/AppRouter.jsx';
 import '@/styles/index.css';
-import { LanguageProvider } from '@/contexts/languageContext';
+import { LanguageProvider } from '@/contexts/LanguageContext';
 import "./i18n";
 import { ThemeProvider } from './contexts/ThemeContext.jsx';
 createRoot(document.getElementById('root')).render(
